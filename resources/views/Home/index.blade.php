486009889093f6fdaa58612fa2055a72678f7163
Federico4610 <rico.nababan46@gmail.com>
2021-08-06 09:50:04 +0700

    commit 1 - UAS PIA

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <div class="card bg-light">
+        <div class="card-body">
+        <h1 style ="text-align:center;">WELCOME TO MY PROFILE</h1>
+        </div>
+        <meta charset="UTF-8">
+        <img src="cic.png" class="img-thumbnail">
+        <title>Home<</title>
+        <style>
+
+        body {
+
+            background-color: #fff;
+
+        }
+        </style>
+    </head>
+    <body>
+    </div>
+    </body>
+    </html>
+@endsection
